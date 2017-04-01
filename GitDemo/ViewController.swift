cf1cf7b8f7ff7b082b71a8996927df0b17cb0a9c
StoneCX <chengxcv5@163.com>
2017-04-01 16:41:57 +0800

    make conflict
    change the file with master branch will conflict

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		print("there add something ")
+		print("there add somethi ")
 	}
 
 	override func didReceiveMemoryWarning() {
